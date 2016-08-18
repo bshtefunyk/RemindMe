3c3eaf5e10888978b3357b06fda0e46a1d495b1b
Bohdan Shtefunyk <bshtefunyk@gmail.com>
2016-08-18 15:47:01 +0300

    111

--- RemindMe2/app/src/main/java/com/example/bohdanshtefunyk/remindme/MainActivity.java
+++ RemindMe2/app/src/main/java/com/example/bohdanshtefunyk/remindme/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.bohdanshtefunyk.remindme;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,10 +9,21 @@ import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initNavigationView();
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+
+
     }
 
     @Override
